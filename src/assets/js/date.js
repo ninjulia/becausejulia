ae21d6ca2bfadea20bf7fb597fd8e99bfe963cc4
Julia <60198903+ninjulia@users.noreply.github.com>
2022-11-23 15:23:52 -0500

    set up grid as a template, minor css updates

--- src/assets/js/date.js
+++ src/assets/js/date.js
@@ -1,4 +1,4 @@
 //////////////////////////////////Get Year for copyright date
-const copyrightDate = new Date;
+const copyrightDate = new Date();
 let copyrightYear = copyrightDate.getFullYear();
 document.getElementById("year").innerHTML = copyrightYear;
--- src/assets/js/jribbble.js
+++ src/assets/js/jribbble.js
@@ -1,34 +1,30 @@
 //////////////////////////////////Latest Work from Dribbble
 jribbble.setToken("9abe753478a2afe894caf7454fb22d9008891cc96538a0c1b47aeeb7b68efe0d");
-const gridInner = document.querySelector('#latestGrid');
 
 //
-//procure shots - dribbble returns 12  
+//procure shots - dribbble returns 12
 //
- jribbble.shots(function(shotsArray) {
-  //where to put the shots
-  const gridInner = document.querySelector('#latestGrid');
-  const imageArray = [];
-  
-  
-  // wrap in required html
-  shotsArray.forEach(shot => {
-    const slideImage = `<div class="grid-item"><a class="stretched-link" href="${shot.html_url}" target="_blank"><img src="${shot.images.normal}" class="img-fluid rounded-1" alt="${shot.title}"></a></div>`;
+jribbble.shots(function (shotsArray) {
+	//where to put the shots
+	const gridInner = document.querySelector("#latest-grid");
+	const imageArray = [];
 
-    //push to array
-    imageArray.push(slideImage);    
-  });  
+	// wrap in required html
+	shotsArray.forEach((shot) => {
+		const slideImage = `<div class="grid-item"><img src="${shot.images.normal}" class="img-fluid rounded-1" alt="${shot.title}"></div>`;
 
-  //add array of items to grid
-  gridInner.innerHTML = imageArray.join(''); 
+		//push to array
+		imageArray.push(slideImage);
+	});
 
- });
+	//add array of items to grid
+	gridInner.innerHTML = imageArray.join("");
+});
 
 //
 //view more shots
 //
-
-jribbble.user(function(userObj) {
-  const gridSubText = `<p class="text-end">View more posts from <a href="${userObj.html_url}" target="_blank" class="text-white">${userObj.login} on dribbble.</a></p>`;
-  document.getElementById("viewMore").innerHTML = gridSubText;
+jribbble.user(function (userObj) {
+	const gridSubText = `<p class="text-end text-white mb-0 pb-0">View more posts from <a href="${userObj.html_url}" target="_blank" class="text-white">${userObj.login} on dribbble.</a></p>`;
+	document.getElementById("viewMore").innerHTML = gridSubText;
 });
