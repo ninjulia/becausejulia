18399eda4dce7a9c80ed7f89b704ae5a3e31a97b
Julia <60198903+ninjulia@users.noreply.github.com>
2023-03-19 12:33:29 -0400

    404 redirect

--- public/assets/js/jribbble.js
+++ public/assets/js/jribbble.js
@@ -3,7 +3,10 @@ let imageArray = [],
 	imageObj = {};
 
 //set up jribbble, return required data
-jribbble.setToken(`${jribbbleAPI}`);
+jribbble.setToken(
+	//"9abe753478a2afe894caf7454fb22d9008891cc96538a0c1b47aeeb7b68efe0d"
+	`${meta.jribbble}`
+);
 
 jribbble.shots(function (shotsArray) {
 	//get src & alt text for each
--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,4 +1,5 @@
 //set prod / dev environment variable
 module.exports = {
 	env: process.env.ELEVENTY_ENV,
+	jribbble: process.env.JRIBBBLE_API_KEY,
 };
