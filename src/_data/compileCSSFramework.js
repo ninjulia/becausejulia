5bcfb4d42a32c60cf4c30aea847203c56749ab33
Julia <60198903+ninjulia@users.noreply.github.com>
2023-08-02 17:32:59 -0400

    - finessed linklist design

--- src/_data/compileCSSFramework.js
+++ src/_data/compileCSSFramework.js
@@ -1,42 +1,36 @@
 //following tutorial from https://dev.to/brewhousedigital/using-purgecss-with-bootstrap-and-eleventy-j7p
 
-const fs = require("fs");
-const MinifyCSS = require("clean-css");
-const postCSS = require("postcss");
-const purgeCSS = require("@fullhuman/postcss-purgecss");
+const fs = require('fs');
+const MinifyCSS = require('clean-css');
+const postCSS = require('postcss');
+const purgeCSS = require('@fullhuman/postcss-purgecss');
 
 module.exports = async function () {
 	// You must create the folder structure first.
 	// WriteFile does not create files if parent folders are missing
-	if (!fs.existsSync("public")) {
-		fs.mkdirSync("public");
+	if (!fs.existsSync('public')) {
+		fs.mkdirSync('public');
 	}
-	if (!fs.existsSync("public/assets/css")) {
-		fs.mkdirSync("public/assets/css");
+	if (!fs.existsSync('public/assets/css')) {
+		fs.mkdirSync('public/assets/css');
 	}
 
 	// Create a custom, purged, version of css
-	const sourceCSS = "src/assets/css/style.css";
-	const destinationCSS = "public/assets/css/style.css";
+	const sourceCSS = 'src/assets/css/style.css';
+	const destinationCSS = 'public/assets/css/style.css';
 	// Add in your file types here
-	const sourceContent = [
-		"src/**/*.njk",
-		"src/**/*.html",
-		"src/**/*.md",
-		"src/**/*.liquid",
-		"src/**/*.js",
-	];
+	const sourceContent = ['src/**/*.njk', 'src/**/*.html', 'src/**/*.md', 'src/**/*.liquid', 'src/**/*.js'];
 
 	//exclude components.njk from prod css
-	if (process.env.ELEVENTY_ENV === "prod") {
-		filterUnused = ["src/**/components.njk"];
+	if (process.env.ELEVENTY_ENV === 'prod') {
+		filterUnused = ['src/**/components.njk'];
 	} else {
 		filterUnused = [];
 	}
 
 	fs.readFile(sourceCSS, (err, css) => {
 		postCSS([
-			require("autoprefixer"),
+			require('autoprefixer'),
 			// Purge CSS will scan through and remove the styles that aren't in your project
 			// skippedContentGlobs filter out components.njk on prod
 			purgeCSS({
@@ -46,28 +40,30 @@ module.exports = async function () {
 				keyframes: true,
 				transitions: true,
 				safelist: [
-					"text-bg-deep-purple",
-					"text-bg-primary",
-					"text-bg-deep-blue",
-					"text-bg-mid-blue",
-					"text-bg-cerulean",
-					"text-bg-teal",
-					"text-bg-green",
-					"text-bg-light-green",
-					"text-bg-lime",
-					"text-bg-danger",
-					"text-bg-info",
-					"text-bg-light",
-					"text-bg-deeper-purple",
-					"btn-deep-purple",
-					"btn-deep-blue",
-					"btn-mid-blue",
-					"btn-cerulean",
-					"btn-teal",
-					"btn-green",
-					"btn-light-green",
-					"btn-lime",
-					"btn-brand-gradient",
+					'text-bg-deep-purple',
+					'text-bg-primary',
+					'text-bg-deep-blue',
+					'text-bg-mid-blue',
+					'text-bg-cerulean',
+					'text-bg-teal',
+					'text-bg-green',
+					'text-bg-light-green',
+					'text-bg-lime',
+					'text-bg-danger',
+					'text-bg-info',
+					'text-bg-light',
+					'text-bg-deeper-purple',
+					'btn-deep-purple',
+					'btn-deep-blue',
+					'btn-mid-blue',
+					'btn-cerulean',
+					'btn-teal',
+					'btn-green',
+					'btn-light-green',
+					'btn-lime',
+					'btn-brand-gradient',
+					'ms-7',
+					'gap-5',
 				],
 			}),
 		])
@@ -79,10 +75,10 @@ module.exports = async function () {
 				let newCSS = result.css;
 
 				// Minify
-				let compiledCSS = new MinifyCSS().minify(newCSS)["styles"];
+				let compiledCSS = new MinifyCSS().minify(newCSS)['styles'];
 
 				// Save
-				fs.writeFileSync(destinationCSS, compiledCSS, { encoding: "utf8" });
+				fs.writeFileSync(destinationCSS, compiledCSS, { encoding: 'utf8' });
 			})
 			.catch((error) => {
 				console.log(error);
