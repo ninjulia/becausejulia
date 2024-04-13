10050271742cca77ca7296b2450bc23428653887
Julia <60198903+ninjulia@users.noreply.github.com>
2024-04-13 15:19:46 -0400

    design code section

---
+++ src/_data/code.js
@@ -0,0 +1,64 @@
+module.exports = async function () {
+  return [
+    {
+      headline: "Email with Video",
+      title: "Email Code",
+      live: "https://ninjulia.github.io/email_html5video/",
+      repo: "https://github.com/ninjulia/email_html5video/",
+      image:
+        "https://raw.githubusercontent.com/ninjulia/email_html5video/main/screenshot.png",
+      type: "Code",
+      tags: "code",
+    },
+    {
+      headline: "Bundle Builder Tool",
+      title: "Front End Web | CMS",
+      live: "https://ninjulia.github.io/bundleBuilder/",
+      repo: "https://github.com/ninjulia/bundleBuilder/",
+      image:
+        "https://raw.githubusercontent.com/ninjulia/bundleBuilder/master/screenshot.png",
+      type: "Code",
+      tags: "code",
+    },
+    {
+      headline: "Speed Visualizer",
+      title: "Front End Web | JQuery",
+      live: "https://ninjulia.github.io/JavaScript_SpeedVisualizer/",
+      repo: "https://github.com/ninjulia/JavaScript_SpeedVisualizer/",
+      image:
+        "https://raw.githubusercontent.com/ninjulia/JavaScript_SpeedVisualizer/main/screenshot.png",
+      type: "Code",
+      tags: "code",
+    },
+    {
+      headline: "Portfolio Template",
+      title: "Front End Web | CMS",
+      live: "https://astro-storyblock-portfolio-template.netlify.app/",
+      repo: "https://github.com/ninjulia/astro-storyblok-portfolio",
+      image:
+        "https://raw.githubusercontent.com/ninjulia/astro-storyblok-portfolio/main/screenshot.png",
+      type: "Code",
+      tags: "code",
+    },
+    {
+      headline: "Email with Scratch-off Effect",
+      title: "Email Code",
+      live: "https://ninjulia.github.io/email_scratchoff/",
+      repo: "https://github.com/ninjulia/email_scratchoff",
+      image:
+        "https://raw.githubusercontent.com/ninjulia/email_scratchoff/main/screenshot.PNG",
+      type: "Code",
+      tags: "code",
+    },
+    {
+      headline: "Email with Carousel",
+      title: "Email Code",
+      live: "https://ninjulia.github.io/email_carousel/",
+      repo: "https://github.com/ninjulia/email_carousel",
+      image:
+        "https://raw.githubusercontent.com/ninjulia/email_carousel/main/screenshot.png",
+      type: "Code",
+      tags: "code",
+    },
+  ];
+};
